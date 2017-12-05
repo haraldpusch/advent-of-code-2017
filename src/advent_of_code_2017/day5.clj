727a7da16c8e8bd72035004865abdf2e1e4f3c77
Harald Pusch <harald.pusch@adesso.de>
2017-12-05 11:28:45 +0100

    add solution for day5

---
+++ src/advent_of_code_2017/day5.clj
@@ -0,0 +1,30 @@
+(ns advent-of-code-2017.day5
+  (:require [clojure.string :as strs]))
+
+(defn read-input [uri]
+  (let [s (strs/trim (slurp uri))
+        lines (strs/split-lines s)]
+    (vec
+      (map
+        #(Integer/valueOf (str %))
+        lines))))
+
+(defn create-step-fn [increment-fn]
+  (fn [col pos iteration]
+    (let [v (nth col pos)
+          col (assoc col pos (increment-fn v))
+          pos (+ pos v)]
+      (if (>= pos (count col))
+        (inc iteration)
+        (recur col pos (inc iteration))))))
+
+(defn solve-puzzle-1 []
+  (let [col (read-input "resources/day5.txt")]
+    ((step-fn inc) col 0 0)))
+
+(defn solve-puzzle-2 []
+  (let [col (read-input "resources/day5.txt")]
+    ((step-fn #(if (> % 2) (dec %) (inc %))) col 0 0)))
+
+(defn solve []
+  (println (format "Day 5 - solution 1: %d - solution 2: %d" (solve-puzzle-1) (solve-puzzle-2))))
