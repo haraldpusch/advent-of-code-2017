f3d85b298609c894e62e2d3a4977db2de03bf076
Harald Pusch <haraldpusch@web.de>
2017-12-06 23:28:01 +0100

    add days 3 and 4

---
+++ src/advent_of_code_2017/day3.clj
@@ -0,0 +1,85 @@
+(ns advent-of-code-2017.day3
+  (require [clojure.string :refer [trim]]))
+
+(defn abs [i] (Math/abs i))
+
+(defn go [{:keys [:x :y :direction] :as m}]
+  (case direction
+    :left  (assoc m :x (dec x))
+    :right (assoc m :x (inc x))
+    :up    (assoc m :y (inc y))
+    :down  (assoc m :y (dec y))))
+
+(defn turn? [{:keys [:x :y]}]
+  (let [ax (abs x)
+        ay (abs y)]
+    (cond
+      (and (= ax ay) (> x 0) (< y 0)) false
+      (= ax ay) true
+      (and (= ax (+ ay 1)) (> x 0) (< y 0)) true
+      :else false
+      )))
+
+(defn turn! [{direction :direction :as m}]
+  (assoc m :direction
+    (case direction
+      :left :down
+      :down :right
+      :right :up
+      :up :left)))
+
+(defn step [{:keys [:direction :x :y :index] :as m}]
+  (-> m
+      (assoc :index (inc index))
+      (go)
+      (#(if (turn? %) (turn! %) %))))
+
+(defn solve-puzzle-1 [col]
+  (let [{x :x y :y} (first (filter
+                             #(= 265149 (:index %))
+                             col))]
+    (+ (abs x) (abs y))))
+
+(defn get-adjacent-positions [{:keys [:x :y] :as m}]
+  [{:x (inc x) :y y}
+   {:x (dec x) :y y}
+   {:x x :y (inc y)}
+   {:x x :y (dec y)}
+   {:x (inc x) :y (inc y)}
+   {:x (inc x) :y (dec y)}
+   {:x (dec x) :y (inc y)}
+   {:x (dec x) :y (dec y)}])
+
+(defn find-first-value [{:keys [:x :y]} col index]
+  (let [v (first (filter #(and (= (:x %) x) (= (:y %) y)) col))]
+    (if
+      (or (nil? v) (> (:index v) index))
+      0
+      (:value v))))
+
+(defn calculate-value [{:keys [:x :y :index] :as in} col]
+  (let [neighbors (get-adjacent-positions in)]
+    (assoc in :value
+      (reduce +
+              (map
+                #(find-first-value % col index)
+                neighbors)))))
+
+(defn wrapped [in col]
+  (let [v (calculate-value in col)]
+    (println v)
+    v))
+
+(defn solve-puzzle-2 [col]
+  (let [col (take 400 (iterate step {:x 1 :y 1 :index 3 :direction :left}))
+        col (cons {:x 1 :y 0 :index 2 :direction :left :value 1} col)]
+    (first
+      (filter #(> (:value %) 265149)
+              (reduce
+                #(conj %1 (wrapped %2 %1))
+                [{:x 0 :y 0 :index 1 :direction :left :value 1}]
+                col)))))
+
+(defn solve []
+  (let [col (iterate step {:x 1 :y 1 :index 3 :direction :left})]
+    (println (format "Day 3 - solution 1: %d - solution 2: %d" (solve-puzzle-1 col) (solve-puzzle-2 col)))))
---
+++ src/advent_of_code_2017/day4.clj
@@ -0,0 +1,37 @@
+(ns advent-of-code-2017.day4
+  (require [clojure.string :refer [trim] :as strs]))
+
+(defn get-input []
+  (map
+    #(strs/split % #"\s")
+    (->
+      (slurp "resources/day4.txt")
+      trim
+      strs/split-lines)))
+
+(defn compare-to-set [col]
+  (= (count col) (count (set col))))
+
+(defn solve-puzzle-1 []
+  (count
+    (filter
+      compare-to-set
+      (get-input))))
+
+(defn contains-no-anagram [col]
+  (=
+    (count col)
+    (count
+      (for [a col
+            b col
+            :when (= (sort a) (sort b))]
+        true))))
+
+(defn solve-puzzle-2 []
+  (count
+    (filter
+      contains-no-anagram
+      (get-input))))
+
+(defn solve []
+  (println (format "Day 4 - solution 1: %d - solution 2: %d" (solve-puzzle-1) (solve-puzzle-2))))
